dc757791d92b6952d740e1076e4ae8940109a8ce
tommin <tomas.mineika@outlook.com>
2020-01-08 23:26:37 +0200

    added heavy weather warning

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const myApp = angular.module("myApp", []);
 const apiKey = "194333f5b09188fbda8c4a3bbfea30b2";
+const heavyWeatherArray = [
+  "heavy thunderstorm",
+  "thunderstorm with heavy rain",
+  "thunderstorm with heavy drizzle",
+  "heavy shower rain and drizzle",
+  "heavy intensity drizzle rain",
+  "heavy intensity rain",
+  "very heavy rain",
+  "extreme rain",
+  "heavy intensity shower rain",
+  "Heavy snow",
+  "Heavy shower snow",
+  "tornado"
+];
 
 myApp
   .controller("weatherController", ($scope, $http) => {
@@ -15,6 +29,9 @@ myApp
     );
 
     $scope.getRequest = () => {
+      $scope.warningStyle = () => {
+        return { display: "none" };
+      };
       $http
         .get(
           "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -42,9 +59,18 @@ myApp
         )
         .then(
           response => {
+            console.log(response);
             $scope.forecast = response;
-            console.log($scope.cityName);
-            console.log($scope.forecast);
+            response.data.list.forEach(element => {
+              element.weather.forEach(el => {
+                if (heavyWeatherArray.includes(el.description)) {
+                  $scope.condition = el.description;
+                  $scope.warningStyle = () => {
+                    return { display: "block" };
+                  };
+                }
+              });
+            });
           },
           err => {
             alert("Error! Wrong city name or no reponse from API");
